b8d18fe5dc3441cd1fcc0a20ded5582dac34a60e
MTtankkeo <ttankkeo112@gmail.com>
2025-07-17 16:07:46 +0900

    commit for isolatedModules

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 export { HTTPConnection } from "./http/http_connection";
 export { HTTPException } from "./http/http_exception";
-export { HTTPHandler, HTTPHandlerCallback, HTTPHandlerListener } from "./http/http_handler";
+export { HTTPHandler } from "./http/http_handler";
 export { HTTPRouter } from "./http/http_router";
+export type { HTTPHandlerCallback, HTTPHandlerListener } from "./http/http_handler";
 
 export { Socket } from "./socket/socket";
+export type { SocketHandlerListener } from "./socket/socket";
 
 export { API } from "./api/api";
 export { APIError } from "./api/api_error";
