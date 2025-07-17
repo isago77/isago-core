f21c787feef1abfa587d36e670d5626221aa5a85
MTtankkeo <ttankkeo112@gmail.com>
2025-07-17 16:04:02 +0900

    export type 으로 변경

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,7 @@
-export { HTTPConnection } from "./http/http_connection";
-export { HTTPException } from "./http/http_exception";
-export { HTTPHandler, HTTPHandlerCallback, HTTPHandlerListener } from "./http/http_handler";
-export { HTTPRouter } from "./http/http_router";
-export { Socket } from "./socket/socket";
-export { API } from "./api/api";
-export { APIError } from "./api/api_error";
+export type { HTTPConnection } from "./http/http_connection";
+export type { HTTPException } from "./http/http_exception";
+export type { HTTPHandler, HTTPHandlerCallback, HTTPHandlerListener } from "./http/http_handler";
+export type { HTTPRouter } from "./http/http_router";
+export type { Socket } from "./socket/socket";
+export type { API } from "./api/api";
+export type { APIError } from "./api/api_error";
